361fe4f6795a860eb5915705b8ac0c076203c734
ankush.nakaskar <ankush.nakaskar@phonepe.com>
2021-12-12 09:20:52 +0530

    first commit

---
+++ src/main/java/org/example/ConcatAndStoreInMysql.java
@@ -0,0 +1,42 @@
+
+package org.example;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.functions;
+
+import java.util.Properties;
+//import static org.apache.spark.sql.functions.concat;
+//import static org.apache.spark.sql.functions.lit;
+
+/**
+ * @author ankush.nakaskar
+ * @version 1.0
+ * @date 11/12/21 5:29 PM
+ */
+public class ConcatAndStoreInMysql {
+
+    public static void main(String[] args) {
+        SparkSession sparkSession = SparkSession.builder()
+                .appName("Concat And Store In MysqL")
+                .master("local")
+                .getOrCreate();
+        Dataset<Row> df = sparkSession.read().format("csv")
+                .option("header", "true")
+                .load("data/authors.csv");
+        df = df.withColumn(
+                "name",
+                functions.concat(df.col("lname"), functions.lit(", "), df.col("fname")));
+
+        df.show();
+        String dbConnectionUrl = "jdbc:postgresql://localhost/test";
+        Properties prop = new Properties();
+        prop.setProperty("driver", "org.postgresql.Driver");
+        df.write()
+                .mode(SaveMode.Overwrite).jdbc(dbConnectionUrl, "ch02", prop);
+    }
+}
+
+
---
+++ src/main/java/org/example/CsvToDataframeApp.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+/**
+ * CSV ingestion in a dataframe.
+ * 
+ * @author jgp
+ */
+public class CsvToDataframeApp {
+
+  /**
+   * main() is your entry point to the application.
+   * 
+   * @param args
+   */
+  public static void main(String[] args) {
+    CsvToDataframeApp app = new CsvToDataframeApp();
+    app.start();
+  }
+
+  /**
+   * The processing code.
+   */
+  private void start() {
+    // Creates a session on a local master
+    SparkSession spark = SparkSession.builder()
+        .appName("Ankush Sample application")
+        .master("local")
+        .getOrCreate();
+
+    // Reads a CSV file with header, called books.csv, stores it in a
+    // dataframe
+    Dataset<Row> df = spark.read().format("csv")
+        .option("header", "true")
+        .load("data/books.csv");
+
+    // Shows at most 5 rows from the dataframe
+    df.show();
+    try {
+      Thread.sleep(35000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+}
